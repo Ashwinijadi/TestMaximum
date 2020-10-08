ac8c97adaeabf5403133cfeb3402d1de19d7f24d
Ashwinijadi <ashwinijadisweety346@gmail.com>
2020-10-08 22:27:55 +0530

    added:Functionality to test maximum number using Generics is added

---
+++ TestMaximum.java
@@ -0,0 +1,8 @@
+package com.capgemini.testmaximum;
+
+public class TestMaximum<T extends Comparable<T>> {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to Maximum Problem Using Generics");
+	}
+}
