3940d8c202c3f29a5227418944a79813c90ca17e
Ashwinijadi <ashwinijadisweety346@gmail.com>
2020-10-08 22:35:04 +0530

    added:Functionality to test maximum number is added with max number at first place

--- TestMaximum.java
+++ TestMaximum.java
@@ -1,8 +1,36 @@
 package com.capgemini.testmaximum;
 
 public class TestMaximum<T extends Comparable<T>> {
+	T x, y, z;
+
+	public TestMaximum(T x, T y, T z) {
+		this.x = x;
+		this.y = y;
+		this.z = z;
+	}
+
+	public T maximum() {
+		return TestMaximum.maximum(x, y, z);
+	}
+
+	public static <T extends Comparable<T>> T maximum(T x, T y, T z) {
+		T max = x;
+		if (y.compareTo(max) > 0) {
+			max = y;
+		}
+		if (z.compareTo(max) > 0) {
+			max = z;
+		}
+		printMax(x, y, z, max);
+		return max;
+	}
+
+	public static <T> void printMax(T x, T y, T z, T max) {
+		System.out.println("maximum is " + max);
+	}
 
 	public static void main(String[] args) {
-		System.out.println("Welcome to Maximum Problem Using Generics");
+		Integer xI = 5, yI = 4, zI = 3;
+		TestMaximum.maximum(xI, yI, zI);
 	}
 }
